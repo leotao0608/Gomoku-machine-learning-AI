a6f24c3081d3764368575811a80e9c7a173d8779
leotao0608 <leotao0608@gmail.com>
2025-06-01 02:20:33 -0600

    Update gomoku_neural_network.py

--- gomoku_neural_network.py
+++ gomoku_neural_network.py
@@ -2,44 +2,59 @@
 #Tensor flow library, numpy
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.layers import Conv2D, Dense, Flatten
 from tensorflow.keras.optimizers import Adam
 import numpy as np
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  
-os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'  
+from tensorflow.keras.callbacks import ReduceLROnPlateau
 
-x_data = np.load('C:/Users/tao/Documents/GitHub/Gomoku AI two algrithms/self play code/dataset-1.1/x_train.npz')
-X_train=x_data["arr_0"]
-y_data = np.load('C:/Users/tao/Documents/GitHub/Gomoku AI two algrithms/self play code/dataset-1.1/y_train.npz')
-Y_train=y_data["arr_0"]
-#(258280, 15, 15)
-X_train = X_train.reshape((-1, 225))  
-Y_train = Y_train.reshape((-1, 225)) 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
-x_train=X_train[:5000]
-y_train=Y_train[:5000]
-#print(X_train[10])
-#print(Y_train[10])
+
+x_data = np.load('C:/Users/Lisaq/Documents/gomoku AI/dataset-1.0/x_train.npz')
+X_train = x_data["arr_0"]  # (258280, 15, 15)
+
+y_data = np.load('C:/Users/Lisaq/Documents/gomoku AI/dataset-1.0/y_train.npz')
+Y_train = y_data["arr_0"]  # (258280, 15, 15)
+
+X_train = X_train[:50000]
+Y_train = Y_train[:50000]
+
+X_train = X_train.reshape((-1, 15, 15, 1)).astype(np.float32)   
+Y_train = Y_train.reshape((-1, 225)).astype(np.float32)       
+
 
 x_data.close()
 y_data.close()
 
+
+reduce_lr = ReduceLROnPlateau(
+    monitor='loss',      
+    factor=0.5,        
+    patience=3,           
+    min_lr=1e-6,        
+    verbose=1             
+)
+
+
 model = Sequential([
-    Flatten(input_shape=(225,)),   
-    Dense(128, activation='relu'), 
-    Dense(128, activation='relu'), 
+    Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same', input_shape=(15, 15, 1)),
+    Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same'),
+    Flatten(),
+    Dense(256, activation='relu'),
     Dense(225, activation='softmax')  
 ])
 
+
 model.compile(
-    optimizer=Adam(learning_rate=0.001),
-    loss='categorical_crossentropy', 
+    optimizer=Adam(learning_rate=0.0005),
+    loss='categorical_crossentropy',
     metrics=['accuracy']
 )
 
-model.fit(X_train, Y_train, epochs=20, batch_size=64)
 
-model.save("C:/Users/tao/Documents/GitHub/Gomoku AI two algrithms/self play code/gomoku_model.h5")
+model.fit(X_train, Y_train, epochs=20, batch_size=64, callbacks=[reduce_lr])
 
 
+model.save("C:/Users/Lisaq/Documents/gomoku AI/gomoku_cnn_model.keras")
